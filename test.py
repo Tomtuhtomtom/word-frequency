054e3df16a549448338e33b3fc4b07a4b0d9ff8e
Thomas Morris <tomchrismorris@yahoo.com>
2022-08-10 19:28:00 -0400

    Mostly working, need to align printout, sort in most used?, hyphens and apostropies not working either

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 # Need to open txt file - check
 # Need to split and lowercase all words - check
-# Need to remove punctuation and stop words - punctuation check - need stop words
+# Need to remove punctuation and stop words - punctuation check
+#    need stop words
 # Need to count number of times each word is used
 # Need to print out word | # & * x #
 
@@ -18,11 +19,19 @@
 # print(type(string.punctuation))
 
 
-# testing
-list_of_words = ["hello", "world", "again", "hello", "world", "a", "the", "hello", "world", "again"]
-
-# for word in list_of_words:
-#     print(f'{word} number of times: {list_of_words.count(word)}')
+# testing - working list and dictionary that counts the words
+# list_of_words = [
+#     "hello",
+#     "world",
+#     "again",
+#     "hello",
+#     "world",
+#     "a",
+#     "the",
+#     "hello",
+#     "world",
+#     "again"
+#         ]
 
 txt_doc = {}
 
@@ -31,3 +40,40 @@ for word in list_of_words:
 
 for key, value in txt_doc.items():
     print(key, " | ", value, ("*" * value))
+
+
+# testing for stop words
+STOP_WORDS = [
+    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
+    'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
+    'will', 'with'
+]
+
+list_of_words = [
+    "hello",
+    "world",
+    "again",
+    "hello",
+    "world",
+    "a",
+    "the",
+    "hello",
+    "world",
+    "again",
+    "the",
+    "a"
+        ]
+
+
+# for word in STOP_WORDS:
+#     if word in list_of_words:
+#         list_of_words.remove(word)
+
+
+# print(list_of_words)
+
+# for word in STOP_WORDS:
+#     list_of_words = list(filter((word).__ne__, list_of_words))
+
+
+# print(list_of_words)
---
+++ testbuild.py
@@ -0,0 +1,27 @@
+import string
+
+STOP_WORDS = [
+    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
+    'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
+    'will', 'with'
+]
+
+with open('praise_song_for_the_day.txt', 'r') as file:
+    doc_string = file.read().replace('\n', ' ')
+    for character in string.punctuation:
+        doc_string = doc_string.replace(character, ' ')
+    doc_words = doc_string.lower().split()
+    for word in STOP_WORDS:
+        doc_words = list(filter((word).__ne__, doc_words))
+
+
+txt_doc = {}
+
+for word in doc_words:
+    txt_doc[word] = doc_words.count(word)
+
+for key, value in txt_doc.items():
+    print(key, " | ", value, ("*" * value))
+
+
+# print(doc_words)
