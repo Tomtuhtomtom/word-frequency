1bc84e22aba7c247236d17382e5c1656b7f1e6f3
Thomas Morris <tomchrismorris@yahoo.com>
2022-08-11 11:26:19 -0400

    Counted and sorted, need to fix alignment on printout

--- word_frequency.py
+++ word_frequency.py
@@ -1,25 +1,44 @@
+
+# needed to import string for the punctuation
 import string
 
+# Given list of stop words
 STOP_WORDS = [
-    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
-    'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
-    'will', 'with'
+    'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has',
+    'he', 'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to',
+    'were', 'will', 'with'
 ]
 
 
+# function that runs when python opens a txt file
+# 'file' takes place of txt file
 def print_word_freq(file):
+    # with open is used to open and close the txt file when done
     with open(file, 'r') as txt_file:
+        # python reads the txt file, replaces \n if necessary
+        # and puts in in doc_string
         doc_string = txt_file.read().replace('\n', ' ')
+        # my_punctuation is string punctuation but keeps the 
+        # apostropes by cutting them out
         my_punctuation = string.punctuation.replace("'", "")
+        # for in goes through and edits out any punctuation 
         for character in my_punctuation:
             doc_string = doc_string.replace(character, ' ')
+        # splitting the string into a list of lowercase words
         doc_words = doc_string.lower().split()
+        # filter out the stop words
         for word in STOP_WORDS:
             doc_words = list(filter((word).__ne__, doc_words))
+    # create an empty dictionary to put words and count in
     txt_doc = {}
+    # create entries in dictionary for each word found and how many
     for word in doc_words:
         txt_doc[word] = doc_words.count(word)
-    for key, value in txt_doc.items():
+    # sorting dictionary
+    sorted_tuples = sorted(txt_doc.items(), key=lambda item: item[1], reverse = True)
+    sorted_dict = {k: v for k, v in sorted_tuples}
+    # use dictiionary to format result printout
+    for key, value in sorted_dict.items():
         stars = "*" * value
         result = f'\t{key} | {value} {stars}'
         print(result.center(20, ' '))
